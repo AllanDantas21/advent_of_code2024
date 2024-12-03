3ff31457be18fbeccd2338185eed412f3b406f21
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-12-03 00:09:21 -0300

    initial file parser

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,9 +1,27 @@
-use std::fs;
+use std::collections::LinkedList;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 
-fn main() -> std::io::Result<()> {
-    let content = fs::read_to_string("text.txt")?;
-    for line in content.lines() {
-        println!("{}", line);
+fn main() -> io::Result<()> {
+    let mut list1: LinkedList<i32> = LinkedList::new();
+    let mut list2: LinkedList<i32> = LinkedList::new();
+
+    let path = Path::new("text.txt");
+    let file = File::open(&path)?;
+    let reader = io::BufReader::new(file);
+
+    for line in reader.lines(){
+        let line = line?;
+        let splits: Vec<&str> = line.split_whitespace().collect();
+
+        if let (Some(&first), Some(&second)) =  (splits.get(0), splits.get(1)) {
+            list1.push_back(first.parse::<i32>().unwrap());
+            list2.push_back(second.parse::<i32>().unwrap());
+        }
     }
+    println!("List 1: {:?}", list1);
+    println!("List 2: {:?}", list2);
+
     Ok(())
 }
