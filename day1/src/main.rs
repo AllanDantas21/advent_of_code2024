e769a1d3d4861a3804dfa4f099e35d5f63c3045e
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-12-03 17:22:06 -0300

    finish day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,27 +1,38 @@
-use std::collections::LinkedList;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+fn get_min_in_list(list: &Vec<i32>) -> &i32 {
+    list.iter().min().unwrap()
+}
+
 fn main() -> io::Result<()> {
-    let mut list1: LinkedList<i32> = LinkedList::new();
-    let mut list2: LinkedList<i32> = LinkedList::new();
+    let mut list1: Vec<i32> = Vec::new();
+    let mut list2: Vec<i32> = Vec::new();
 
     let path = Path::new("input.txt");
     let file = File::open(&path)?;
     let reader = io::BufReader::new(file);
 
-    for line in reader.lines(){
+    for line in reader.lines() {
         let line = line?;
         let splits: Vec<&str> = line.split_whitespace().collect();
 
-        if let (Some(&first), Some(&second)) =  (splits.get(0), splits.get(1)) {
-            list1.push_back(first.parse::<i32>().unwrap());
-            list2.push_back(second.parse::<i32>().unwrap());
+        if let (Some(&first), Some(&second)) = (splits.get(0), splits.get(1)) {
+            list1.push(first.parse::<i32>().unwrap());
+            list2.push(second.parse::<i32>().unwrap());
         }
     }
-    println!("List 1: {:?}", list1);
-    println!("List 2: {:?}", list2);
 
+    let mut total_distance = 0;
+    while !list1.is_empty() && !list2.is_empty() {
+        let min1 = get_min_in_list(&list1);
+        let min2 = get_min_in_list(&list2);
+        let distance = (min1 - min2).abs();
+        total_distance += distance;
+        list1.remove(list1.iter().position(|&x| x == *min1).unwrap());
+        list2.remove(list2.iter().position(|&x| x == *min2).unwrap());
+    }
+    println!("Total distance: {}", total_distance);
     Ok(())
 }
